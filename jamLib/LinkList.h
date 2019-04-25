e994d77c6323954c178e20c6ecea5348bcd9350a
piJam <pijamly@gmail.com>
2019-04-25 16:30:41 +0800

    commit
    commit

--- jamLib/LinkList.h
+++ jamLib/LinkList.h
@@ -18,8 +18,27 @@ protected:
         Node* next;
     };
 
-    mutable Node m_header; //const方法中要修改变量的值，要加mutable修饰
+   // mutable Node m_header; //const方法中要修改变量的值，要加mutable修饰
+    mutable struct : public Object
+    {
+        char reserved [sizeof(T)];
+        Node* next;
+    } m_header;
+
     int m_length;
+
+    Node* position(int i)  //获取要获取元素位置的上一个元素，利用上一个元素的next找到要操作的元素位置
+    {
+        Node* ret =reinterpret_cast<Node*>(&m_header);
+
+        for(int p = 0; p < i ;p++)
+        {
+            ret = ret->next;
+        }
+
+        return ret;
+    }
+
 public:
     LinkList()
     {
@@ -36,12 +55,7 @@ public:
 
             if(node != NULL)
             {
-                Node* current = &m_header;
-
-                for(int p = 0; p < i ;p++)
-                {
-                    current = current->next;
-                }
+                Node* current = position(i);
 
                 node->value = e;
                 node->next = current->next;
@@ -66,11 +80,8 @@ public:
         bool ret = (0 <= i)&&( i < m_length);
         if(ret)
         {
-            Node* current = &m_header;
-            for(int p = 0;p < i;p++)
-            {
-                current = current->next;
-            }
+            Node* current = position(i);
+
             Node* del = current->next;
             current->next = del->next;
             delete del;
@@ -78,16 +89,25 @@ public:
         }
         return 0;
     }
+
+    T get(int i) const
+    {
+        T ret;
+        if(get(i,ret))
+        {
+            return ret;
+        }else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException,"index out if bounds...");
+        }
+    }
+
     bool get(int i,T& e) const
     {
         bool ret = (0 <= i)&&(i < m_length);
         if(ret)
         {
-            Node* current = &m_header;
-            for(int p = 0;p < i; i++)
-            {
-                current = current->next;
-            }
+            Node* current = position(i);
             e = current->next->value;
         }
         return ret;
@@ -97,11 +117,8 @@ public:
         bool ret = (0 <= i)&&(i < m_length);
         if(ret)
         {
-            Node* current = &m_header;
-            for(int p = 0;p < i; i++)
-            {
-                current = current->next;
-            }
+            Node* current = position(i);
+
             current->next->value = e;
         }
         return ret;
