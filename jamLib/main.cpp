6f9b80d185906140cbc8b3539962955b7d16ba69
piJam <pijamly@gmail.com>
2019-05-16 11:39:06 +0800

    Update main.cpp

--- jamLib/main.cpp
+++ jamLib/main.cpp
@@ -13,28 +13,7 @@ using namespace jamLib;
 
 int main()
 {
-    DualLinkList<int> dual;
-    for(int i = 0; i < 10; i++)
-    {
-        dual.insert(i);
-    }
 
-    for(int i = 0; i < 10;i++)
-    {
-        cout<<  dual.get(i) << endl;
-    }
-    cout<< endl;
-     dual.remove(1);
-    for(dual.move(0,1); !dual.end();dual.next() )
-    {
-        cout<< dual.current() << endl;
-    }
-    cout << endl;
-    dual.clear();
-    for(dual.move(0,1); !dual.end(); dual.pre())
-    {
-        cout << dual.current() << endl;
-    }
     return 0;
 }
 
