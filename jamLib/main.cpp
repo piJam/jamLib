65d1a35fbc9ad840be98ff2af72ac32a03484c0d
piJam <pijamly@gmail.com>
2019-05-23 20:27:20 +0800

    1
    1

--- jamLib/main.cpp
+++ jamLib/main.cpp
@@ -6,23 +6,10 @@
 
 using namespace std;
 using namespace jamLib;
+
+
 int main()
 {
-
-    StaticStack<int,10> list;
-
-    for(int i = 0; i < list.capacity(); i++)
-    {
-        list.push(i);
-    }
-
-    for(int i = 0; i < list.capacity(); i++)
-    {
-
-        cout<< list.top() <<endl;
-        list.pop();
-    }
-
 
 
 
