43915ee48013f45c50f149b889348e89f2fba35f
piJam <pijamly@gmail.com>
2019-05-24 11:29:14 +0800

    c
    c

--- jamLib/Exception.h
+++ jamLib/Exception.h
@@ -21,19 +21,22 @@ private:
     char getCharCode(int num);
 
 #endif
-
-
     void init(const char* message,const char* file,int line);
 public:
     Exception(const char* message);
+
     Exception(const char* file,int line);
+
     Exception(const char* message,const char* file,int line);
 
     Exception(const Exception& e);
+
     Exception& operator =(const Exception& e);
 
     virtual const char* message() const;
+
     virtual const char* location() const;
+
     virtual ~Exception() = 0;
 
 
@@ -44,11 +47,15 @@ class ArithmeticException : public Exception
 {
 public:
     ArithmeticException():Exception(0){}
+
     ArithmeticException(const char* message):Exception(message){}
+
     ArithmeticException(const char* file,int line):Exception(file,line){}
+
     ArithmeticException(const char* message,const char* file,int line):Exception(message,file,line){}
 
     ArithmeticException(const ArithmeticException& e):Exception(e){}
+
     ArithmeticException& operator =(const ArithmeticException& e)
     {
         Exception::operator =(e);
