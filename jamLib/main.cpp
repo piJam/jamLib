cb3be0e2600a9f3b021faf5d1d0df18e538eeb69
piJam <49149280+piJam@users.noreply.github.com>
2019-06-12 16:35:30 +0800

    pmt
    pmt

--- jamLib/main.cpp
+++ jamLib/main.cpp
@@ -5,18 +5,42 @@
 using namespace std;
 using namespace jamLib;
 
+int* make_pmt(const char* p)
+{
+    int len = strlen(p);
+    int* ret = static_cast<int*>(malloc(sizeof(int) * len));
+    int longestLength = 0;
+    ret[0] = 0;
+
+    for(int i = 1; i<len ; i++)
+    {
+        while( longestLength > 0 && p[longestLength] != p[i] )
+        {
+            longestLength = ret[ longestLength - 1];
+        }
+
+        if(p[longestLength] == p[i])
+        {
+            longestLength++;
+        }
+        ret[i] = longestLength;
+    }
+    return ret;
+}
 
 
 int main()
 {
-    JString str = " BCGD  EFR**90_--";
-    cout<< str.lenght() << endl;
-    cout << str.trim().str() << endl;
-    cout << str.lenght() << endl;
 
 
+    char* p = "ABABDABA";
 
+    int* ret = make_pmt(p);
 
+    for(int i = 0; i< strlen("ABABDABA"); i++)
+    {
+        cout<< i << ":" << ret[i] << endl;
+    }
     return 0;
 }
 
