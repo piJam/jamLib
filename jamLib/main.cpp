55a787ad0ffdda828a6d41c9350878c355832099
piJam <pijamly@gmail.com>
2019-06-22 19:55:48 +0800

    com
    com

--- jamLib/main.cpp
+++ jamLib/main.cpp
@@ -5,6 +5,77 @@
 using namespace std;
 using namespace jamLib;
 
+struct Node
+{
+    int m_value;
+    Node* next;
+};
+
+Node* create_list(int v, int len)
+{
+    Node* ret = NULL;
+    Node* slider =NULL;
+
+    for(int i=0; i<len; i++)
+    {
+        Node* n = new Node();
+        n->m_value = v++;
+        n->next = NULL;
+
+        if( slider == NULL)
+        {
+            slider = n;
+            ret = n;
+        }
+        else
+        {
+            slider->next = n;
+            slider = n;
+        }
+    }
+    return ret;
+}
+
+void print_list(Node* list)
+{
+    while(list)
+    {
+        cout << list->m_value << " -> ";
+
+        list = list->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void delete_list(Node* list)
+{
+    while(list)
+    {
+        Node* temp = list;
+        list = list->next;
+        delete temp;
+    }
+}
+
+Node* reverse(Node* list)
+{
+    if( list == NULL || list->next == NULL )
+    {
+        return list;
+    }
+    else
+    {
+        Node* guard = list->next;
+        Node* ret = reverse(list->next);
+
+        guard->next = list;
+
+        list->next = NULL;
+        return ret;
+    }
+
+
+}
 int sum(int num)
 {
 
@@ -20,10 +91,12 @@ int sum(int num)
 
 int main()
 {
-    JString s = "absdhasdwerwerwerwer";
 
-    cout<< sum(1000) << endl;
+    Node* node = create_list(0, 10);
+    print_list(node);
 
+    print_list(reverse(node));
+    delete_list(node);
     return 0;
 }
 
