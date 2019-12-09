bd2691caaecdfbf5568c7965385ef4af8b8d3ff8
pijamly <pijamly@gmail.com>
2019-12-09 21:15:24 +0800

    update

--- jamLib/main.cpp
+++ jamLib/main.cpp
@@ -7,30 +7,11 @@
 using namespace std;
 using namespace jamLib;
 
-class A
-{
-public:
-    A()
-    {
-        cout << "A init()"<< this << endl;
-    }
 
-    A( const A& a)
-    {
-        cout << "copy A" << this << endl;
-    }
-
-};
-
-void show( A a)
-{
-    cout << &a << endl;
-}
 
 int main()
 {
-    A a;
-    show(a);
+
 
     return 0;
 }
